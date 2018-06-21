e25961b5f22e5548d9168a70e776c8f5c699999b
marcos.olivio <marcos.olivio@azi.com.br>
2018-06-21 14:10:01 -0400

    Criacao do projeto

---
+++ src/main/java/com/treinamento/ApiTreinamentoApplication.java
@@ -0,0 +1,12 @@
+package com.treinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTreinamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTreinamentoApplication.class, args);
+    }
+}
